2a66d2a3e10c2506541d5e5a42b3b2142224c4f6
ibrahim bulut <ibobulut1997@gmail.com>
2020-11-01 16:47:54 +0300

    ilk commit

---
+++ mern-exercise-tracker/backend/models/user.model.js
@@ -0,0 +1,20 @@
+//const { Schema, Mongoose, model } = require("mongoose");
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlenght: 3
+    },
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
